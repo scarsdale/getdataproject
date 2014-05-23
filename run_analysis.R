06d532f93b25c1d545e0cf69d21e3538b6981ac1
student <scarsdale@users.noreply.github.com>
2014-05-24 02:01:53 +0900

    create final tidy data set and write to csv file

--- run_analysis.R
+++ run_analysis.R
@@ -65,14 +65,13 @@ tokenize.label <- function(label) {
     # fewer words than the rest
     do.call(rbind, strsplit(label, "-", fixed=T))
 }
-isfeaturemean <- function(label) {
-    tok <- tokenize.label(label)
-    tok[,2] == "mean()"
+doesfeaturematch <- function(re) {
+    function(label) {
+        sapply(label, function(s) { length(grep(re, s)) != 0 })
+    }
 }
-isfeaturesd <- function(label) {
-    tok <- tokenize.label(label)
-    tok[,2] == "std()"
-}
+isfeaturemean <- doesfeaturematch("-mean\\(\\)$")
+isfeaturesd <- doesfeaturematch("-std\\(\\)$")
 isfeaturemeanorsd <- function(label) {
     isfeaturemean(label) | isfeaturesd(label)
 }
@@ -85,11 +84,14 @@ xy.file <- function(dataset) {
 get.measurements <- function(pathfn, n=-1) {
     # read a full dataset (test or training)
     features <- get.colnames()
-    read.fwf(getrawdata(pathfn("X")),
-             widths=rep(nchar(" -1.0000000e+000"),length(features)),
-             header=F,
-             col.names=features,
-             n=n)             
+    ret <- read.fwf(getrawdata(pathfn("X")),
+                    widths=rep(nchar(" -1.0000000e+000"),length(features)),
+                    header=F,
+                    col.names=features,
+                    n=n)
+    # re-set the column names because read.fwf truncates them
+    colnames(ret) <- features
+    ret
 }
 extract.mean.sd <- function(dat) {
     # subset data frame to only the mean / std dev measurements
@@ -107,12 +109,61 @@ get.subjects <- function(pathfn, n=-1) {
     factor(subjects[,1])
 }
 make.intermediate.data <- function(pathfn, n=-1) {
+    # make an intermediate data set consisting of
+    # the mean and std dev measurements with activity and subject added
     dat <- extract.mean.sd(get.measurements(pathfn, n=n))
     dat$Activity <- get.activities(pathfn, n=n)
     dat$Subject <- get.subjects(pathfn, n=n)
     dat
 }
 make.merged.intermediate <- function(n=-1) {
+    # merge the training and test sets into one intermediate data set
     rbind(make.intermediate.data(xy.file("train"), n=n),
           make.intermediate.data(xy.file("test"), n=n))
 }
+subject.activity.averages <- function(dat) {
+    # take an intermediate data set and reduce it to each variable's
+    # overall mean for a given subject,activity pair
+    measurecolnames <- colnames(dat)[isfeaturemean(colnames(dat))]
+    ret <- do.call(data.frame,
+                   lapply(measurecolnames,
+                          function (feature) {
+                              tapply(dat[,feature],
+                                     list(dat$Subject, dat$Activity))
+                          }))
+    colnames(ret) <- measurecolnames
+    ret <- cbind(ret, data.frame(dat$Activity, dat$Subject))
+    ret
+}
+simplify.names <- function(labels) {
+    # remove the 'Mag-mean()' suffix from measurement labels,
+    # since we are reporting only the overall means of vector magnitudes
+    # also remove dat. prefix from Activity and Subject and
+    # deduplicate BodyBody to Body
+    sapply(labels, function(s) {
+        if (s %in% c("dat.Activity", "dat.Subject"))
+            substr(s, 5, nchar(s))
+        else
+            sub("BodyBody",
+                "Body",
+                substr(s, 1, nchar(s) - nchar("Mag-mean()")),
+                fixed=T)
+    })
+}
+expand.names <- function(labels) {
+    # expand Acc -> Acceleration and Gyro -> AngularVelocity
+    # remove t prefix and replace f prefix with FrequencyOf
+    ret <- sub("Acc", "Acceleration", labels, fixed=T)
+    ret <- sub("Gyro", "AngularVelocity", ret, fixed=T)
+    ret <- sub("^t", "", ret)
+    sub("^f", "FrequencyOf", ret)
+}
+make.tidy <- function(dat) {
+    ret <- subject.activity.averages(dat)
+    colnames(ret) <- expand.names(simplify.names(colnames(ret)))
+    # reorder columns so subject and activity come first
+    ret[,c(ncol(ret), ncol(ret) - 1, seq(1, ncol(ret) - 2))]
+}
+write.csv(make.tidy(make.merged.intermediate()),
+          "tidy.csv",
+          row.names=F)
